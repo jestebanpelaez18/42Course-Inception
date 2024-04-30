86098f3ddd1e2020d85d5bc7afaefef791052db0
jestebanpelaez18 <juanes.pelaez18@gmail.com>
2024-04-30 12:20:13 +0300

    small change in mariadb sh

--- srcs/src/mariadb/script/mariadb.sh
+++ srcs/src/mariadb/script/mariadb.sh
@@ -12,16 +12,31 @@ chown -R mysql:mysql /var/lib/mysql
 # init database
 mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
 
+# his part of the script starts the MariaDB server in bootstrap mode, which allows it to execute SQL commands without requiring authentication.
+# mysqld --user=mysql --bootstrap << EOF
+# USE mysql;
+# FLUSH PRIVILEGES;
+
+# ALTER USER 'root'@'localhost' IDENTIFIED BY '12345';
+# CREATE DATABASE mariadb CHARACTER SET utf8 COLLATE utf8_general_ci;
+# CREATE USER 'jpelaez-'@'%' IDENTIFIED by 'hola';
+# GRANT ALL PRIVILEGES ON mariadb.* TO 'jpelaez-'@'%';
+# GRANT ALL PRIVILEGES ON *.* TO 'jpelaez-'@'%' IDENTIFIED BY 'hola' WITH GRANT OPTION;
+# GRANT SELECT ON mysql.* TO 'jpelaez-'@'%';
+
+# FLUSH PRIVILEGES;
+# EOF
+
 mysqld --user=mysql --bootstrap << EOF
 USE mysql;
 FLUSH PRIVILEGES;
 
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$MARIADB_ROOT_PASSWORD';
-CREATE DATABASE $MARIA_DB_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE USER '$MARIA_DB_USER'@'%' IDENTIFIED by '$MARIA_DB_PASSWORD';
-GRANT ALL PRIVILEGES ON $MARIA_DB_NAME.* TO '$MARIA_DB_USER'@'%';
-GRANT ALL PRIVILEGES ON *.* TO '$MARIA_DB_USER'@'%' IDENTIFIED BY '$MARIA_DB_PASSWORD' WITH GRANT OPTION;
-GRANT SELECT ON mysql.* TO '$MARIA_DB_USER'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';
+CREATE DATABASE ${MARIA_DB_NAME} CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER '${MARIA_DB_USER}'@'%' IDENTIFIED by '${MARIA_DB_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${MARIA_DB_NAME}.* TO '${MARIA_DB_USER}'@'%';
+GRANT ALL PRIVILEGES ON *.* TO '${MARIA_DB_USER}'@'%' IDENTIFIED BY '${MARIA_DB_PASSWORD}' WITH GRANT OPTION;
+GRANT SELECT ON mysql.* TO '${MARIA_DB_USER}'@'%';
 
 FLUSH PRIVILEGES;
 EOF
